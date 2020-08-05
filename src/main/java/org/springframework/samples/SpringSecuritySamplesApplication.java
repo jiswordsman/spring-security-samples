6312aa7a7d1483d3437ff3d6cacb7143936079ce
冀拥军 <yongjun.ji@ucarinc.com>
2020-08-05 18:05:16 +0800

    初始化

---
+++ src/main/java/org/springframework/samples/SpringSecuritySamplesApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springframework/samples/SpringSecuritySamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
